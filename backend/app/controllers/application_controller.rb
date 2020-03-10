089621a012b1331ca14b42c29a40bfff27a72464
learn <kmanoorib@gmail.com>
2020-03-10 00:31:32 -0700

    change folder structures to make it more readable

---
+++ backend/app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::API
+    def index
+        topics = Topic.all
+        if topics
+            render json: topics #, include: [:name]
+        else
+            render plain: "topics not found"
+        end
+        # render json: sightings, include: [:bird, :location]
+    end
+end
---
+++ backend/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'application#index'
+  resources :questions
+  resources :topics
+  resources :exams
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
