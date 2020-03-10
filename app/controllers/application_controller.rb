95a2ba265b3287192bcefeb56876ca4ac1473cae
learn <kmanoorib@gmail.com>
2020-03-09 23:51:23 -0700

    can render topics in json or render plain topics not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::API
     def index
         topics = Topic.all
-        @topics = topics
         if topics
             render json: topics #, include: [:name]
         else
-            binding.pry
             render plain: "topics not found"
         end
         # render json: sightings, include: [:bird, :location]
