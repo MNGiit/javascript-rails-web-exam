573e8d55d8b070b3f43990de623911e7e63f175e
learn <kmanoorib@gmail.com>
2020-03-11 00:02:35 -0700

    can fetch topics

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,21 @@
 document.write("Moof")
+
+fetch('http://api.open-notify.org/astros.json')
+.then(function(response) {
+  return response.json();
+}).then(function(json) {
+  console.log(json);
+  console.log(`Holy cow! There are ${json["number"]} humans in space.`);
+});
+
+function fetchTopics() {
+    let topics = fetch('http://localhost:3000/topics')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        console.log(json);
+    })
+}
+
+fetchTopics();
