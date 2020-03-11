8a9cc0517f3530616ddf4c48bc1d93148927bd86
learn <kmanoorib@gmail.com>
2020-03-10 23:46:10 -0700

    topics#index gets topics

--- backend/app/controllers/topics_controller.rb
+++ backend/app/controllers/topics_controller.rb
@@ -1,2 +1,11 @@
 class TopicsController < ApplicationController
+    def index
+        topics = Topic.all
+        if topics
+            render json: topics #, include: [:name]
+        else
+            render plain: "topics not found"
+        end
+        # render json: sightings, include: [:bird, :location]
+    end
 end
