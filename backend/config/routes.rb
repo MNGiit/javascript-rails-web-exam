ad384a7cdb3ce3cda19d497baf9a88a18f40d838
learn <kmanoorib@gmail.com>
2021-02-02 19:57:37 -0800

    test routes for fetchTopics

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root 'application#index'
+
+  # resources :topics               #fetchTopics works with this line
+  # get '/topics' => 'topics#index' #fetchTopics works with this line
+  resources :topics, only: [:index]
+
   resources :questions
-  resources :topics
   resources :exams
-  get '/topics' => 'topics#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
