e563fd097ccf774b17d2ac36d4f4d899f8e061d2
learn <kmanoorib@gmail.com>
2021-03-23 02:20:00 -0700

    placement holder for quiz method

--- backend/app/controllers/exams_controller.rb
+++ backend/app/controllers/exams_controller.rb
@@ -2,4 +2,19 @@ class ExamsController < ApplicationController
     def index
         #
     end
+
+    def quiz
+        # get questions
+        questions = Question.all
+        if questions
+            render json: questions
+        else
+            render plain: "questions not found from exams controller"
+        end
+        # find questions related to topic
+        # if found, render questions
+        # else render exam doesn't have any questions
+        # render plain: "quiz method called from topics controller"
+    end
+
 end
