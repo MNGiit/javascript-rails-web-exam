c684be14abbf5d83cd4f21f83de230eb05c65a27
learn <kmanoorib@gmail.com>
2021-03-24 01:53:49 -0700

    new quiz can randomize choices for each question

--- frontend/quiz.js
+++ frontend/quiz.js
@@ -2,7 +2,6 @@ class Quiz {
     constructor(questions, number) {
         this.questions = [];
         this.setNumberOfQuestions(questions, number);
-        // randomizeAnswersAndCreateKey(this.questions);
         this.answerKey = [];
         this.setAnswerKey();
     }
@@ -20,14 +19,12 @@ class Quiz {
         for(let i = 0; i < this.questions.length; i++) {
             // randomize choices and assign it to choices
             this.questions[i].choices = randomizeChoices(this.questions[i]);
-            
             // find where answer is in randomized choices then add i to answer key
             for (let j = 0; j < this.questions[i].choices.length; j++) {
                 if (this.questions[i].correct == this.questions[i].choices[j]) {
                     this.answerKey.push(j);
                 }
             }
-
             // remove correct, choiceB, choiceC, choiceD
             delete questions[i].correct;
             delete questions[i].choiceB;
@@ -37,9 +34,25 @@ class Quiz {
     }
 
     randomizeChoices(question) {
-        return question;
+        let choices = getChoices(question);
+        for (let i = 0; i < choices.length; i++) {
+            let ran = Math.floor(Math.random() * (i + 1));
+            let temp = choices[i];
+            choices[ran] = temp;
+        }
+        return choices;
+    }
+
+    getChoices(question) {
+        let choices = [];
+        choices.push(question.correct);
+        choices.push(question.choiceB);
+        choices.push(question.choiceC);
+        choices.push(question.choiceD);
+        return choices;
     }
 }
+
 
 
 // const tenQuestions = 10; // eliminate "magic number" in code
