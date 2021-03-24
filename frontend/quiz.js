4672714e9153823d252dc5c8b54959e553946748
learn <kmanoorib@gmail.com>
2021-03-24 01:44:33 -0700

    new quiz can set it's own answer key

--- frontend/quiz.js
+++ frontend/quiz.js
@@ -3,6 +3,8 @@ class Quiz {
         this.questions = [];
         this.setNumberOfQuestions(questions, number);
         // randomizeAnswersAndCreateKey(this.questions);
+        this.answerKey = [];
+        this.setAnswerKey();
     }
 
     setNumberOfQuestions(questions, number) {
@@ -12,6 +14,30 @@ class Quiz {
         for(let i = 0; i < number; i++) {
             this.questions.push(questions[i])
         }
+    }
+
+    setAnswerKey() {
+        for(let i = 0; i < this.questions.length; i++) {
+            // randomize choices and assign it to choices
+            this.questions[i].choices = randomizeChoices(this.questions[i]);
+            
+            // find where answer is in randomized choices then add i to answer key
+            for (let j = 0; j < this.questions[i].choices.length; j++) {
+                if (this.questions[i].correct == this.questions[i].choices[j]) {
+                    this.answerKey.push(j);
+                }
+            }
+
+            // remove correct, choiceB, choiceC, choiceD
+            delete questions[i].correct;
+            delete questions[i].choiceB;
+            delete questions[i].choiceC;
+            delete questions[i].choiceD;           
+        }
+    }
+
+    randomizeChoices(question) {
+        return question;
     }
 }
 
