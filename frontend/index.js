3b3d532789faf062dd6beaa2a7ec8bb68696e5dd
learn <kmanoorib@gmail.com>
2021-04-01 23:54:04 -0700

    quiz instance handles result display

--- frontend/index.js
+++ frontend/index.js
@@ -260,7 +260,7 @@ function nextQuestion() {
 
 function retakeQuiz() {
     quiz.retakeQuiz();
-    document.getElementById("results").style.display = "none";
+    // document.getElementById("results").style.display = "none";
 }
 
 function pickAnswer(selected) {
